e391b1e77dc4dcd6fbc333d3783972ead8b6e8df
Oscar Mata <omata@applaudostudios.com>
2020-09-13 13:15:35 -0600

    :lipstick: added styles for sidebar

--- swapi-web/src/components/SideBar.tsx
+++ swapi-web/src/components/SideBar.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import {useQuery} from '@apollo/client';
 import { ALL_PEOPLE } from '../helpers/queries';
-import { Person } from '../helpers/interfaces';
+import { DetailsInterface } from '../helpers/interfaces';
 
 import chevron from '../assets/icons/chevron.svg';
 
@@ -25,9 +25,14 @@ export const SideBar = ({setSelected}: {setSelected: any}) => {
 
 	return (
 			<div className="people-cell-container">
-				{people.map((element: Person, index: number) => (
+				{people.map((element: DetailsInterface, index: number) => (
 					<button key={index} onClick={() => setSelected(element.id)}>
-						<p>{element.name}</p>
+						<div className="cell-title">
+							<p className="name">{element.name}</p>
+							<p className="sub-details">
+								{element.species?.name || 'Human'} from {element.homeworld.name}
+							</p>
+						</div>
 						<img src={chevron} alt="chevron right" />
 					</button>
 				))}
